e5185aefe9773577bf62bafd5cdac1121c608d2a
chris <chrismarais@protonmail.com>
2019-11-20 16:58:17 +0200

    requirements and setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+ 
+setup(
+    name='empowerment',       # This is the name of your PyPI-package.
+    version='0.1',          # Update the version number for new releases
+    description='Compute the information-theoretic empowerment in toy worlds',
+    url='http://github.com/Mchristos/empowerment',
+    author='Chris',
+    license='MIT',
+    #author_email='',
+    #setup_requires=['pytest-runner'],
+    #tests_require=['pytest'],
+    #packages=['']
+)
